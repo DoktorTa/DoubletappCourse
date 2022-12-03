fefa8d664df1bc172d46d8240b26dd967c088936
DoktorTa <55731358+DoktorTa@users.noreply.github.com>
2022-12-03 15:13:01 +0500

    Start app

--- HabitTracker/app/src/main/java/course/doubletapp/habittracker/MainActivity.kt
+++ HabitTracker/app/src/main/java/course/doubletapp/habittracker/MainActivity.kt
@@ -2,10 +2,14 @@ package course.doubletapp.habittracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import course.doubletapp.habittracker.databinding.HabitItemBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: HabitItemBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = HabitItemBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
