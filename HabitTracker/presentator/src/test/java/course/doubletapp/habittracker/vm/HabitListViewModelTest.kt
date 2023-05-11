40ba02b1311e03d714247fe6ac66dff84efc8d75
DoktorTa <55731358+DoktorTa@users.noreply.github.com>
2023-05-11 18:30:55 +0500

    Last homework

---
+++ HabitTracker/presentator/src/test/java/course/doubletapp/habittracker/vm/HabitListViewModelTest.kt
@@ -0,0 +1,34 @@
+package course.doubletapp.habittracker.vm
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class HabitListViewModelTest {
+
+    @org.junit.jupiter.api.Test
+    fun getInfoText() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getNowFilters() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun applyFilters() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun searchByName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun searchByDescription() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun searchByPriority() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun resetFilter() {
+    }
+}
